7ce6bca3e582e27f0369f7d3bdd42029358e6395
yudith1234 <107668606+yudith1234@users.noreply.github.com>
2024-10-28 03:19:37 -0500

    initial comint

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- view/footer.php -->
+</main>
+<footer class="text-center mt-5">
+    <p>© 2024 VILLATRUCKS S.A.C.</p>
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!-- view/header.php -->
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VILLATRUCKS S.A.C.</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../estilos/styles.css">
+</head>
+
+<body>
+    <header class="text-center">
+        <h1>VILLATRUCKS S.A.C.</h1>
+
+    </header>
+    <main class="container mt-5"></main>
